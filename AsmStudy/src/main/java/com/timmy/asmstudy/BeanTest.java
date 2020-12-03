214fed42b8b57b675d8748ba8c28f5dd0cf6d6cc
Timmy <timmy_zzh@163.com>
2020-12-04 07:34:32 +0800

    ASM字节码动态生成类和字段

---
+++ AsmStudy/src/main/java/com/timmy/asmstudy/BeanTest.java
@@ -0,0 +1,19 @@
+package com.timmy.asmstudy;
+
+//package com.timmy.asmstudy;
+//import com.sun.istack.internal.NotNull;
+//
+//public class Person {
+//    @NotNull
+//    public String name;
+//}
+
+/**
+ * 动态生成如上诉Person类
+ */
+public class BeanTest {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ AsmStudy/src/main/java/com/timmy/asmstudy/Person.java
@@ -0,0 +1,9 @@
+package com.timmy.asmstudy;
+
+import com.sun.istack.internal.NotNull;
+
+public class Person {
+
+    @NotNull
+    public String name;
+}
